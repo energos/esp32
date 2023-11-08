cb525275105f48c011f12ffe420b85fbe57eead4
energos <energos@users.noreply.github.com>
2023-11-08 02:13:51 -0300

    Simple Led Blinking example

---
+++ 01_blink-led/main/01_blink-led.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN GPIO_NUM_2
+
+void app_main(void)
+{
+  gpio_reset_pin(LED_PIN);
+  gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+  printf("Hello, World!\n");
+  while (1)
+    {
+      gpio_set_level(LED_PIN, 1);
+      printf("LED ON\n");
+      vTaskDelay(pdMS_TO_TICKS(500));
+
+      gpio_set_level(LED_PIN, 0);
+      printf("LED OFF\n");
+      vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
