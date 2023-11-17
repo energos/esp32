0239517c06bbdb2d0be0b0615047dcac943bb884
energos <energos@users.noreply.github.com>
2023-11-17 11:20:15 -0300

    01_blink-led/: Use the menuconfig infrastructure

--- 01_blink-led/main/01_blink-led.c
+++ 01_blink-led/main/01_blink-led.c
@@ -3,8 +3,8 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define LED_PIN GPIO_NUM_2
-#define LED_DELAY pdMS_TO_TICKS(500)
+#define LED_PIN CONFIG_LED_PIN
+#define LED_DELAY_TICKS pdMS_TO_TICKS(CONFIG_LED_DELAY)
 
 void app_main(void)
 {
@@ -16,10 +16,10 @@ void app_main(void)
     {
       gpio_set_level(LED_PIN, 1);
       printf("LED ON\n");
-      vTaskDelay(LED_DELAY);
+      vTaskDelay(LED_DELAY_TICKS);
 
       gpio_set_level(LED_PIN, 0);
       printf("LED OFF\n");
-      vTaskDelay(LED_DELAY);
+      vTaskDelay(LED_DELAY_TICKS);
     }
 }
