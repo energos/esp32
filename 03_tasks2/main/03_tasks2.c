0a2d083f186eef11322f1b5fc4afd418a6781ea9
energos <energos@users.noreply.github.com>
2023-11-21 16:38:10 -0300

    03_tasks2/: Simple Tasks example, part 2

---
+++ 03_tasks2/main/03_tasks2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED1_PIN CONFIG_LED1_PIN
+#define LED2_PIN CONFIG_LED2_PIN
+#define LED_DELAY_TICKS pdMS_TO_TICKS(CONFIG_LED_DELAY)
+
+
+TaskHandle_t myTask1Handle = NULL;
+TaskHandle_t myTask2Handle = NULL;
+
+void task1(void *arg)
+{
+  uint8_t led_state = 0;
+  while (1)
+    {
+      gpio_set_level(LED1_PIN, led_state);
+      led_state = !led_state;
+      vTaskDelay(LED_DELAY_TICKS);
+    }
+}
+
+void task2(void *arg)
+{
+  uint8_t led_state = 0;
+  while (1)
+    {
+      gpio_set_level(LED2_PIN, led_state);
+      led_state = !led_state;
+      vTaskDelay(LED_DELAY_TICKS);
+    }
+}
+
+void app_main(void)
+{
+  gpio_reset_pin(LED1_PIN);
+  gpio_set_direction(LED1_PIN, GPIO_MODE_OUTPUT);
+
+  gpio_reset_pin(LED2_PIN);
+  gpio_set_direction(LED2_PIN, GPIO_MODE_OUTPUT);
+
+  printf("Hello, World!\n");
+
+  xTaskCreate(task1, "task1", 4096, NULL, 10, &myTask1Handle);
+  xTaskCreate(task2, "task2", 4096, NULL, 10, &myTask2Handle);
+
+  while (1)
+    {
+      printf("Stayin' Alive...\n");
+      vTaskDelay(10 * LED_DELAY_TICKS);
+    }
+}
